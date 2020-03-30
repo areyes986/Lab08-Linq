c4a253b0fbc0cf51d42d1972c8d76e5a8c411a10
Allyson Reyes <areyes986@gmail.com>
2020-03-30 10:41:03 -0700

    structure is created

---
+++ LINQ/LINQ/Classes/Geometry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ.Classes
+{
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using LINQ.Classes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadJson();
+        }
+
+        static void ReadJson()
+        {
+            using (StreamReader reader = File.OpenText(@"../../../data.json"))
+            {
+                string data = reader.ReadToEnd();
+                RootObject newData = JsonConvert.DeserializeObject<RootObject>(data);
+
+                foreach (var item in newData.features)
+                {
+                    Console.WriteLine(item.properties.neighborhood);
+                }
+            }
+        }
+
+    }
+}
